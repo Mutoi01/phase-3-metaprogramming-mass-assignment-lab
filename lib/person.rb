a1d8b52670a2c85f8818dfdd91f8755fd37e8a98
Mutoi01 <pngando9@gmail.com>
2022-09-06 11:14:50 +0300

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
   # your code here
+  attr_accessor :name, :birthday, :eye_color, :height,:hair_color ,:handed, :complexion, :weight, :t_shirt_size, :wrist_size,  :pant_length, :pant_width,:glove_size
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
 end
